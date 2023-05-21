5c0abd4379f7752be3c27a73fcc4e35d40a23456
FLSoz <65517812+FLSoz@users.noreply.github.com>
2023-05-21 14:12:39 -0700

    Initial commit

---
+++ EnhancedBlockInfoDisplayMod.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnhancedBlockInfoDisplay
+{
+    public class EnhancedBlockInfoDisplayMod : ModBase
+    {
+        private const string HarmonyID = "com.flsoz.ttmods.EnhancedBlockInfoDisplay";
+        internal static Harmony harmony;
+        public override void DeInit()
+        {
+            harmony.UnpatchAll(HarmonyID);
+        }
+
+        public override void Init()
+        {
+            harmony = new Harmony(HarmonyID);
+            harmony.PatchAll();
+        }
+    }
+}
